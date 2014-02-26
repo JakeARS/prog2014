c6ad7f4b33a1c27ea3ca7dec891d4378977a4e0b
JakeARS <arsentiev.evgeny@gmail.com>
2014-02-26 23:57:03 +0600

    NewProgram v2.0

--- NewApp/NewApp/Form1.Designer.cs
+++ NewApp/NewApp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
@@ -48,11 +49,18 @@
             this.button2 = new System.Windows.Forms.Button();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -84,7 +92,7 @@
             this.checkBox2.TabIndex = 3;
             this.checkBox2.Text = "Стиральная машина";
             this.checkBox2.UseVisualStyleBackColor = true;
-            
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox3
             // 
@@ -95,7 +103,7 @@
             this.checkBox3.TabIndex = 4;
             this.checkBox3.Text = "СВЧ печь";
             this.checkBox3.UseVisualStyleBackColor = true;
-            
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
             // 
             // checkBox4
             // 
@@ -106,7 +114,7 @@
             this.checkBox4.TabIndex = 5;
             this.checkBox4.Text = "Плита";
             this.checkBox4.UseVisualStyleBackColor = true;
-            
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
             // 
             // label2
             // 
@@ -135,6 +143,7 @@
             0,
             0,
             0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // numericUpDown2
             // 
@@ -152,6 +161,7 @@
             0,
             0,
             0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // numericUpDown3
             // 
@@ -169,6 +179,7 @@
             0,
             0,
             0});
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
             // 
             // numericUpDown4
             // 
@@ -186,6 +197,7 @@
             0,
             0,
             0});
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
             // 
             // label3
             // 
@@ -257,7 +269,7 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(241, 228);
+            this.button1.Location = new System.Drawing.Point(241, 239);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 17;
@@ -268,7 +280,7 @@
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(322, 228);
+            this.button2.Location = new System.Drawing.Point(322, 239);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 18;
@@ -293,22 +305,47 @@
             this.label10.TabIndex = 20;
             this.label10.Text = "0";
             // 
-            // button3
+            // pictureBox1
             // 
-            this.button3.Location = new System.Drawing.Point(297, 139);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 21;
-            this.button3.Text = "OK";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.pictureBox1.Location = new System.Drawing.Point(5, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 68);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(77, 123);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(64, 68);
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(5, 197);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(64, 68);
+            this.pictureBox3.TabIndex = 23;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(77, 197);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(64, 68);
+            this.pictureBox4.TabIndex = 24;
+            this.pictureBox4.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(409, 263);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(409, 274);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.button2);
@@ -329,14 +366,18 @@
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
-            this.MinimumSize = new System.Drawing.Size(425, 301);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(425, 310);
             this.Name = "Form1";
             this.Text = "Shop";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -364,7 +405,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- NewApp/NewApp/Form1.cs
+++ NewApp/NewApp/Form1.cs
@@ -29,11 +29,53 @@ namespace NewApp
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
+        void RecalcTotalTable() 
+        {
+            int s = 0, a, b, c, d;
+            if (checkBox1.Checked)
+            {                
+                pictureBox1.Image = Image.FromFile("1.jpg");
+                a = Convert.ToInt32(numericUpDown1.Value);
+                s = s + (a * 17000);
+            }
+            else
+            {
+                pictureBox1.Image = null;
+            }
+            if (checkBox2.Checked)
+            {                
+                pictureBox2.Image = Image.FromFile("2.jpg");
+                b = Convert.ToInt32(numericUpDown2.Value);
+                s = s + (b * 12000);
+            }
+            else
+            {
+                pictureBox2.Image = null;
+            }
+            if (checkBox3.Checked)
+            {                
+                pictureBox3.Image = Image.FromFile("3.jpg");
+                c = Convert.ToInt32(numericUpDown3.Value);
+                s = s + (c * 6000);
+            }
+            else
+            {
+                pictureBox3.Image = null;
+            }
+            if (checkBox4.Checked)
+            {                
+                pictureBox4.Image = Image.FromFile("4.jpg");
+                d = Convert.ToInt32(numericUpDown4.Value);
+                s = s + (d * 8000);
+            }
+            else
+            {
+                pictureBox4.Image = null;
+            }
+            label10.Text = s.ToString();
         }
-       
+
         private void button3_Click(object sender, EventArgs e)
         {
             int s = 0, a, b, c, d;
@@ -69,7 +111,6 @@ namespace NewApp
                 var fileName = sfd.FileName;
                 ShopData pd = new ShopData();
                 pd.ItemType = new List<ShopType>();
-                //pd.ItemType.Add(ShopType.Plita);
                 if (checkBox1.Checked)
                 pd.ItemType.Add(ShopType.Hol);
                 if (checkBox2.Checked)
@@ -78,6 +119,7 @@ namespace NewApp
                 pd.ItemType.Add(ShopType.CVH);
                 if (checkBox4.Checked)
                 pd.ItemType.Add(ShopType.Plita);
+                
 
                 XmlSerializer xs = new XmlSerializer(typeof(ShopData));
                 var fileStream = File.Create(fileName);
@@ -107,41 +149,50 @@ namespace NewApp
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            int s = 0, a, b, c, d;
-            if (checkBox1.Checked)
-            {
-                a = Convert.ToInt32(numericUpDown1.Value);
-                s = s + (a * 17000);
-            }
-            if (checkBox2.Checked)
-            {
-                b = Convert.ToInt32(numericUpDown2.Value);
-                s = s + (b * 12000);
-            }
-            if (checkBox3.Checked)
-            {
-                c = Convert.ToInt32(numericUpDown3.Value);
-                s = s + (c * 6000);
-            }
-            if (checkBox4.Checked)
-            {
-                d = Convert.ToInt32(numericUpDown4.Value);
-                s = s + (d * 8000);
-            }
-            label10.Text = s.ToString();
+            RecalcTotalTable();
         }
 
-      
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
 
-    
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            RecalcTotalTable();
+        }
+
+
+
     }
     public class ShopData
-    {
-        //List<ShopType> ItemType {get;set;}
+    {        
         public List<ShopType> ItemType { get; set; }
-        /*pd.ItemType = new List<ShopType>();
-        pd.ItemType.Add(ShopType.Plita);
-        public List<ShopType> ItemType { get; set; }*/
     }
     public enum ShopType
     {
