08dbe53129140f7f9b60f4b91ab2a91c7aee66e0
Evgeny Arsentiev <arsentiev.evgeny@gmail.com>
2014-05-19 22:58:11 +0600

    My project

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class JABrowser
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JABrowser));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.back = new System.Windows.Forms.ToolStripButton();
+            this.next = new System.Windows.Forms.ToolStripButton();
+            this.refresh = new System.Windows.Forms.ToolStripButton();
+            this.home = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.adress = new System.Windows.Forms.ToolStripTextBox();
+            this.search = new System.Windows.Forms.ToolStripButton();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.back,
+            this.next,
+            this.refresh,
+            this.home,
+            this.toolStripSeparator1,
+            this.adress,
+            this.search});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(720, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // back
+            // 
+            this.back.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.back.Image = ((System.Drawing.Image)(resources.GetObject("back.Image")));
+            this.back.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(23, 22);
+            this.back.Text = "Back";
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // next
+            // 
+            this.next.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.next.Image = ((System.Drawing.Image)(resources.GetObject("next.Image")));
+            this.next.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(23, 22);
+            this.next.Text = "Forward";
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.refresh.Image = ((System.Drawing.Image)(resources.GetObject("refresh.Image")));
+            this.refresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(23, 22);
+            this.refresh.Text = "Refresh";
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // home
+            // 
+            this.home.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.home.Image = ((System.Drawing.Image)(resources.GetObject("home.Image")));
+            this.home.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.home.Name = "home";
+            this.home.Size = new System.Drawing.Size(23, 22);
+            this.home.Text = "Home";
+            this.home.Click += new System.EventHandler(this.home_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // adress
+            // 
+            this.adress.Name = "adress";
+            this.adress.Size = new System.Drawing.Size(500, 25);
+            this.adress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.adress_KeyDown);
+            this.adress.Click += new System.EventHandler(this.adress_Click);
+            // 
+            // search
+            // 
+            this.search.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.search.Image = ((System.Drawing.Image)(resources.GetObject("search.Image")));
+            this.search.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(23, 22);
+            this.search.Text = "Search";
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 25);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(720, 337);
+            this.browser.TabIndex = 1;
+            this.browser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browser_DocumentCompleted);
+            // 
+            // JABrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(720, 362);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JABrowser";
+            this.Text = "JABrowser";
+            this.Load += new System.EventHandler(this.JABrowser_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton back;
+        private System.Windows.Forms.ToolStripButton next;
+        private System.Windows.Forms.ToolStripButton refresh;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripTextBox adress;
+        private System.Windows.Forms.ToolStripButton search;
+        private System.Windows.Forms.ToolStripButton home;
+        private System.Windows.Forms.WebBrowser browser;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class JABrowser : Form
+    {
+        public JABrowser()
+        {
+            InitializeComponent();
+        }        
+        
+        private void search_Click(object sender, EventArgs e)
+        {
+            browser.Navigate(adress.Text);              
+        }
+
+        private void home_Click(object sender, EventArgs e)
+        {
+            browser.Navigate("http://google.com");
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            browser.Refresh();
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            browser.GoBack();
+        }
+
+        private void adress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                browser.Navigate(adress.Text);
+            }
+
+            if (e.KeyCode == Keys.F5)
+            {
+                browser.Refresh();
+            }
+
+            if (e.KeyCode == Keys.Home)
+            {
+                browser.Navigate("http://google.com");
+            }
+        }
+
+        private void JABrowser_Load(object sender, EventArgs e)
+        {
+            browser.Navigate("http://google.com");
+            WindowState = FormWindowState.Maximized;            
+        }
+
+        private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            adress.Text = browser.Url.ToString();
+        }
+
+        private void adress_Click(object sender, EventArgs e)
+        {
+            adress.SelectionStart = 0;
+            adress.SelectionLength = adress.Text.Length;
+        }
+    }
+}
