559d141eb9ee8fba42c018f168a70b8efeccbb6a
Evgeny Arsentiev <arsentiev.evgeny@gmail.com>
2014-06-03 16:45:43 +0600

    My Game ))

---
+++ Game/Game/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.screen = new System.Windows.Forms.Panel();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.screen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.player);
+            this.screen.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screen.Location = new System.Drawing.Point(0, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(465, 288);
+            this.screen.TabIndex = 0;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.InitialImage = ((System.Drawing.Image)(resources.GetObject("player.InitialImage")));
+            this.player.Location = new System.Drawing.Point(216, 122);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 30);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 288);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.screen.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel screen;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Game/Game/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form1 : Form
+    {
+
+        bool right;
+        bool left;
+        bool jump;
+        int G = 30;
+        int Force;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (right == true)
+            {
+                player.Left += 5;
+            }
+            if (left == true)
+            {
+                player.Left -= 5;
+            }
+
+            if (jump == true) 
+            {//falling
+                player.Top -= Force;
+                Force -= 1;
+            }
+
+            if (player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height;   //stop falling at bottom
+                jump = false;                
+            }
+            else
+            {
+                player.Top += 5;   //falling
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = true;
+                player.Image = Image.FromFile("walk_r.gif");
+            }
+            
+            if (e.KeyCode == Keys.Left)
+            {
+                left = true;
+                player.Image = Image.FromFile("walk_l.gif");
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }  
+
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    player.Image = Image.FromFile("jump.png");
+                    jump = true;
+                    Force = G;
+                }
+
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                right = false;
+                player.Image = Image.FromFile("stand.png");
+            }
+            
+            if (e.KeyCode == Keys.Left)
+            {
+                left = false;
+                player.Image = Image.FromFile("stand.png");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            player.Image = Image.FromFile("stand.png");
+        }
+    }
+}
